7ce491758125433f7b2ae67586d983bed64cdf8f
dima123dm <dimamariamss@gmail.com>
2026-04-11 20:04:21 +0300

    Auto-update from Mriamys Agent Tracker

--- check_logs.py
+++ check_logs.py
@@ -9,11 +9,8 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try:
     client.connect(HOST, username=USER, password=PASS)
-    stdin, stdout, stderr = client.exec_command("cd /root/Discord_mriamys && source venv/bin/activate && python3 -c 'import nacl.secret; print(nacl.__version__)'")
-    out = stdout.read().decode('utf-8')
-    err = stderr.read().decode('utf-8')
-    print("OUT:", out)
-    print("ERR:", err)
+    stdin, stdout, stderr = client.exec_command("journalctl -u mriamys -n 40 --no-pager")
+    print(stdout.read().decode('utf-8'))
 except Exception as e:
     print(e)
 finally:
